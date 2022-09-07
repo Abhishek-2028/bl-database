b95b5ed6693a54c99c50ceefd6100c8cee721265
Abhishek <dhananjaykolaskar2001@gmail.com>
2022-09-07 11:08:34 +0530

    emp-id remove

--- model/Daily_update.js
+++ model/Daily_update.js
@@ -6,11 +6,8 @@ var tasks = new mongoose.Schema({
       task:{
         type: String,
         required: true
-      },
-      emp_id:{
-        type:String,
-        required: true
       }
+    
 })
 
 module.exports = mongoose.model("Daily_update",tasks);
