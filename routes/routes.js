a9f320a900751f6b1c83fb000e92d3a69edc5857
Abhishek <dhananjaykolaskar2001@gmail.com>
2022-09-12 15:48:46 +0530

    image routes changes

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,7 @@ const emp_controller= require("../controllers/emp_controller")
 var bodyParser = require("body-parser");
 var jsonParser = bodyParser.json();
 const multer = require("multer")
-const express = require("express");
-const app = express();
-
+const express =require("express")
 
 
 var storage = multer.diskStorage({
@@ -31,10 +29,10 @@ router.post("/task-post/:id",emp_controller.task_post)
 router.get("/task-get",emp_controller.task_get)
 router.delete("/task-del/:id",emp_controller.task_del)
 router.post("/emp-login",jsonParser,emp_controller.emp_login)
-app.use('/user_docs', express.static('uploads')); 
+router.use('/user_docs', express.static('uploads')); 
 router.post("doc_upload/:id", upload.single('user_docs'), emp_controller.docs_post)
 router.get("/getdocs",emp_controller.docs_get)
-app.use('/user_file', express.static('uploads'));
+router.use('/user_file', express.static('uploads'));
 
 
 
