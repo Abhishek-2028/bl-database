4b1b3ad01cd42c35182e53ea15710e054dd0b124
Abhishek <dhananjaykolaskar2001@gmail.com>
2022-09-13 11:40:54 +0530

    image

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,7 @@ const multer = require("multer")
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads/')
+        cb(null)
     },
     filename: function (req, file, cb) {
 
