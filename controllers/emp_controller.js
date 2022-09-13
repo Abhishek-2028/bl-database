f3401f929888a92884a77f305fefd64f602495fb
Abhishek <dhananjaykolaskar2001@gmail.com>
2022-09-13 11:12:54 +0530

    image issue

--- controllers/emp_controller.js
+++ controllers/emp_controller.js
@@ -201,7 +201,7 @@ const docs_post = async(req,res) =>{
       
         Doc_name: req.body.Doc_name,
         
-        Doc_img: `http://localhost:${port}/user_file/${req.file.originalname}`,
+        Doc_img: `http://localhost:5555/user_file/${req.file.originalname}`,
 
         emp_doc_id: req.params.id
    
